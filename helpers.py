9a32add4580aa54bf771b674b3998ffaa591ca02
Artem <pakshaartem@gmail.com>
2021-11-28 15:37:42 +0300

    add func to read and validate float nums

--- helpers.py
+++ helpers.py
@@ -13,3 +13,25 @@ def merge_dicts(first, second, third):
         merged_dict = merge_dicts(first, second)
         merged_dict = merge_dicts(merged_dict, third)
     return merged_dict
+
+
+def read_and_check(welcome_text="", params_count=1, param_names=[]):
+    input_list = input(welcome_text+" ").split()
+    while len(input_list) < params_count:
+        print("Вы ввели слишком малое кол-во параметров. Попробуйте снова.")
+        input_list = input(welcome_text).split()
+
+    output_arr = []
+
+    for i in range(params_count):
+        curr = input_list[i]
+        while True:
+            try:
+                curr_float = float(curr)
+                output_arr.append(curr_float)
+                break
+            except ValueError:
+                curr_param_name = f"Параметр №{i+1}" if len(param_names) == 0 else param_names[i]
+                input_curr = input(curr_param_name+" не является числом. Введите его заново: ").split()
+                curr = input_curr[0] if len(input_curr) else ""
+    return output_arr if params_count > 1 else output_arr[0]
