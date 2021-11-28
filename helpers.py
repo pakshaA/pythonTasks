77044f3a52a552c589246ac566107fd7d145b54e
Artem <pakshaartem@gmail.com>
2021-11-28 15:04:41 +0300

    add merge func for python less 3.5

--- helpers.py
+++ helpers.py
@@ -1,7 +1,15 @@
+import sys
+
+
 def merge_dicts(first, second, third):
-    merged_dict = {
-        **first,
-        **second,
-        **third
-    }
+    version = sys.version_info
+    if version.major >= 3 and version.minor >= 5:
+        merged_dict = {
+            **first,
+            **second,
+            **third
+        }
+    else:
+        merged_dict = merge_dicts(first, second)
+        merged_dict = merge_dicts(merged_dict, third)
     return merged_dict
