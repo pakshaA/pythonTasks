643a6979627b6a8f22106fed10aa14a0c15a5ba4
Artem <pakshaartem@gmail.com>
2021-11-28 16:03:01 +0300

    add output type param in func read_and_check

--- helpers.py
+++ helpers.py
@@ -15,7 +15,7 @@ def merge_dicts(first, second, third):
     return merged_dict
 
 
-def read_and_check(welcome_text="", params_count=1, param_names=[]):
+def read_and_check(welcome_text="", params_count=1, param_names=[], output_type="str"):
     input_list = input(welcome_text+" ").split()
     while len(input_list) < params_count:
         print("Вы ввели слишком малое кол-во параметров. Попробуйте снова.")
@@ -32,6 +32,6 @@ def read_and_check(welcome_text="", params_count=1, param_names=[]):
                 break
             except ValueError:
                 curr_param_name = f"Параметр №{i+1}" if len(param_names) == 0 else param_names[i]
-                input_curr = input(curr_param_name+" не является числом. Введите его заново: ").split()
+                input_curr = input(curr_param_name+f" не является типом {output_type}. Введите его заново: ").split()
                 curr = input_curr[0] if len(input_curr) else ""
     return output_arr if params_count > 1 else output_arr[0]
